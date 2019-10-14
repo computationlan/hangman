b770001f0957fb198bcb48d9b44010dc917c1875
Lidia <computationlan@gmail.com>
2019-10-13 20:38:37 -0700

    cleanup

--- serverHangmanProcess.py
+++ serverHangmanProcess.py
@@ -1,5 +1,4 @@
-import json
-from flask import Flask, request, redirect, session, url_for, escape, render_template
+from flask import Flask, request, session, render_template
 from flask.json import jsonify
 import requests
 import random
@@ -7,105 +6,121 @@ import sqlite3
 
 app = Flask(__name__)
 
-app.secret_key = b'qz~y\9K_CU>&uGYM;xcW'
+app.secret_key = b"qz~y\9K_CU>&uGYM;xcW"
 
-@app.route('/')
+
+def get_db():
+    conn = sqlite3.connect("hangman.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route("/")
 def main_page():
     path = "game_of_hangman.html"
     return render_template(path)
 
-@app.route('/game_init', methods=['GET'])
+
+@app.route("/game_init")
 def game_init():
-    print('this is game init')
-    if request.method == 'GET':
-        username = request.args.get('username')
-        difficulty = request.args.get('difficulty')
-        if 'challenge_id' in request.args:
-            session['challenge_id'] = request.args.get('challenge_id')
-            conn = sqlite3.connect('hangman.db')
-            cur = conn.cursor()
-            cur.execute('SELECT challenge, difficulty, score, username FROM challenge_share WHERE share_id = ?', (session['challenge_id'],))
-            row = cur.fetchall()
-            session['challenge'] = row[0][0]
-            session['difficulty'] = row[0][1]
-            session['friend_score'] = row[0][2]
-        else:
-            print('no challenge_id is in')
-            response = requests.get("http://app.linkedin-reach.io/words?difficulty="+difficulty)
-            session['challenge'] = random.choice(response.text.split('\n')).upper()
-        session['username'] = username
-        session['difficulty'] = difficulty
-        session['wrong_guesses_left'] = 6
-        #response = requests.get("http://app.linkedin-reach.io/words?difficulty="+difficulty)
-        #print(response.status_code)
-        #print(len(response.text))
+    username = request.args.get("username")
+    difficulty = request.args.get("difficulty")
+    challenge_id = request.args.get("challenge_id")
+    session.clear()
+    if challenge_id:
+        cur = get_db().cursor()
+        cur.execute(
+            " SELECT challenge, difficulty, score, username FROM challenge_share WHERE share_id = ? ",
+            (challenge_id,),
+        )
+        row = cur.fetchone()
+        # add error handling if not found in db
+        session["challenge_id"] = challenge_id
+        session["challenge"] = row["challenge"]
+        session["difficulty"] = row["difficulty"]
+        session["friend_score"] = row["score"]
+    else:
+        # add some error message if nothing is returned on the request - either connection issue or there are no words
+        response = requests.get("http://app.linkedin-reach.io/words?difficulty=" + difficulty)
+        app.logger.debug((response.status_code, len(response.text)))
+        session["challenge"] = random.choice(response.text.split("\n")).upper()
+        session["difficulty"] = difficulty
+    session["username"] = username
+    # keyboard should also respond to real actual keyboard, not just on screen buttons
+    session["guessed"] = ["*"] * len(session["challenge"])
+    session["wrong_guesses_left"] = 6
+    session["wrong_guesses"] = []
+    app.logger.debug(session)
+    game_json = {
+        "guessed": session["guessed"],
+        "wrong_guesses_left": session["wrong_guesses_left"],
+        "wrong_guesses": [],
+        "game_over": False,
+        "game_over_message": "",
+    }
+    return jsonify(game_json)
 
-        #add some error message if nothing is returned on the request - either connection issue or there are no words
-        #keyboard should also respond to real actual keyboard, not just on screen buttons
 
-        #print(random.choice(response.text.split('\n')))
-        session['guessed'] = ['*']*len(session['challenge'])
-        session['wrong_guesses'] = []
-        game_json = {'guessed': session['guessed'], 'wrong_guesses_left': session['wrong_guesses_left'], 'game_over':False}
-        print(session)
-    return json.dumps(game_json)#'New game has started!'
-
-@app.route('/guess_handler')
+@app.route("/guess_handler")
 def guess_handler():
-    if request.method == 'GET':
-        letter = request.args.get('letterGuessed')
-        print('begin', letter, session)
-        game_over = False
-        share_id = None
-        game_over_message = ''
-        if letter not in session['challenge'] and letter not in session['wrong_guesses']:
-            session['wrong_guesses_left'] -= 1
-            if session['wrong_guesses_left'] == 0:
-                game_over = True
-                game_over_message = 'Sorry, you lost! Try again!'
-            wrong_guesses = session['wrong_guesses']
-            wrong_guesses.append(letter)
-            session['wrong_guesses'] = wrong_guesses
-        else:
-            currently_guessed = session['guessed']
-            for n, each in enumerate(session['challenge']):
-                if letter == each:
-                    currently_guessed[n] = letter
-            session['guessed'] = currently_guessed
-            if ''.join(currently_guessed) == session['challenge']:
-                game_over = True
-                game_over_message = 'Awesome! You won!'
-                conn = sqlite3.connect('hangman.db')
-                sql = ' UPDATE leaderboard SET score = ? , username = ? WHERE difficulty = ? and score < ? '
-                cur = conn.cursor()
-                cur.execute(sql, (100*session['wrong_guesses_left'], session['username'], session['difficulty'], 100*session['wrong_guesses_left']))
-                conn.commit()
-
-                conn = sqlite3.connect('hangman.db')
-                sql = ' INSERT INTO challenge_share (challenge, difficulty, score, username) VALUES(?,?,?,?) '
-                cur = conn.cursor()
-                cur.execute(sql, (session['challenge'], session['difficulty'], 100*session['wrong_guesses_left'], session['username']))
-                share_id = cur.lastrowid
-                conn.commit()
-        print(session)
-    guess_json = {'guessed': session['guessed'], 'wrong_guesses_left': session['wrong_guesses_left'], 'wrong_guesses': session['wrong_guesses'], 'game_over':game_over, 'game_over_message':game_over_message}
+    letter = request.args.get("letterGuessed")
+    game_over = False
+    share_id = None
+    game_over_message = ""
+    if letter not in session["challenge"]:
+        session["wrong_guesses_left"] -= 1
+        wrong_guesses = session["wrong_guesses"]
+        wrong_guesses.append(letter)
+        session["wrong_guesses"] = wrong_guesses
+    else:
+        currently_guessed = session["guessed"]
+        for n, each in enumerate(session["challenge"]):
+            if letter == each:
+                currently_guessed[n] = letter
+        session["guessed"] = currently_guessed
+    if "".join(session["guessed"]) == session["challenge"]:
+        game_over = True
+        game_over_message = "Awesome! You won!"
+        score = 100 * session["wrong_guesses_left"]
+        conn = get_db()
+        cur = conn.cursor()
+        sql = " UPDATE leaderboard SET score = ?, username = ? WHERE difficulty = ? and score < ? "
+        cur.execute(sql, (score, session["username"], session["difficulty"], score))
+        sql = " INSERT INTO challenge_share (challenge, difficulty, score, username) VALUES(?, ?, ?, ?) "
+        cur.execute(
+            sql,
+            (session["challenge"], session["difficulty"], score, session["username"]),
+        )
+        share_id = cur.lastrowid
+        conn.commit()
+    elif session["wrong_guesses_left"] == 0:
+        game_over = True
+        game_over_message = "Sorry, you lost! Try again!"
+    guess_json = {
+        "guessed": session["guessed"],
+        "wrong_guesses_left": session["wrong_guesses_left"],
+        "wrong_guesses": session["wrong_guesses"],
+        "game_over": game_over,
+        "game_over_message": game_over_message,
+    }
     if game_over:
-        guess_json['challenge'] = session['challenge']
+        guess_json["challenge"] = session["challenge"]
     if share_id:
-        guess_json['share_id'] = share_id
-        guess_json['friend_name'] = session['username']
-    return json.dumps(guess_json)#'Your guess was ' + str(letter)
+        guess_json["share_id"] = share_id
+        guess_json["friend_name"] = session["username"]
+    app.logger.debug(session)
+    app.logger.debug(guess_json)
+    return jsonify(guess_json)
 
-@app.route('/leaderboard')
+
+@app.route("/leaderboard")
 def get_leaderboard():
-    conn = sqlite3.connect('hangman.db')
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    cur.execute('SELECT difficulty, score, username FROM leaderboard order by difficulty desc')
- 
+    cur = get_db().cursor()
+    sql = " SELECT difficulty, score, username FROM leaderboard ORDER BY difficulty DESC "
+    cur.execute(sql)
     rows = cur.fetchall()
-    
-    return json.dumps([dict(row) for row in rows])
+    return jsonify([dict(row) for row in rows])
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000)
---
+++ static/hangman.js
@@ -0,0 +1,95 @@
+function updateGameState(response) {
+  var jsonResponse = JSON.parse(response);
+  console.log(jsonResponse);
+  refreshTree(jsonResponse["wrong_guesses_left"]);
+  document.getElementById("challenge").innerHTML = jsonResponse["guessed"].join(" ");
+  drawAlphabet(jsonResponse["wrong_guesses"], jsonResponse["guessed"], jsonResponse["game_over"]);
+  document.getElementById("game_over").innerHTML = jsonResponse["game_over_message"];
+  if (jsonResponse["game_over"]) {
+    document.getElementById("challenge").innerHTML = jsonResponse["challenge"];
+    getLeaderboard();
+    if ("share_id" in jsonResponse) {
+      document.getElementById("share_link").innerHTML =
+        '<a href="http://localhost:5000/?share_id=' +
+        jsonResponse["share_id"].toString() +
+        "&friend_name=" +
+        jsonResponse["friend_name"] +
+        '">Want to share this challenge with your friend?</a> <a href="http://localhost:5000/">Or play again?</a>';
+    }
+  } else {
+    document.getElementById("share_link").innerHTML = "";
+  }
+}
+
+function refreshTree(wrong_guesses_left) {
+  var img = document.getElementById("tree");
+  img.src = "/static/tree-image-" + (6 - wrong_guesses_left).toString() + ".png";
+}
+
+function drawAlphabet(wrong_guesses, guessed, game_over) {
+  var alphabet = document.getElementById("alphabet");
+  alphabet.innerHTML = "";
+  for (letter of "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
+    var element = document.createElement("input");
+    element.setAttribute("type", "button");
+    element.setAttribute("value", letter);
+    element.setAttribute("name", letter);
+    element.disabled = game_over;
+    if (wrong_guesses.includes(letter)) {
+      element.style.color = "red";
+      element.disabled = true;
+    } else if (guessed.includes(letter)) {
+      element.style.color = "green";
+      element.disabled = true;
+    }
+    element.onclick = function() {
+      letterGuessed = this.value;
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", "/guess_handler?letterGuessed=" + letterGuessed, true);
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          updateGameState(xhttp.responseText);
+        }
+      };
+      xhttp.send();
+    };
+    alphabet.appendChild(element);
+  }
+}
+
+function gameInit() {
+  const form = document.querySelector("form");
+  const data = new URLSearchParams(new FormData(form).entries());
+  console.log(data);
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "/game_init?" + data, true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      updateGameState(xhttp.responseText);
+    }
+  };
+  xhttp.send();
+}
+
+function getLeaderboard() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "/leaderboard", true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var jsonResponse = JSON.parse(xhttp.responseText);
+      console.log(jsonResponse);
+      var leaderboard = document.getElementById("leaderboard");
+      leaderboard.innerHTML = "";
+      for (row of jsonResponse) {
+        var tr = document.createElement("tr");
+        for (field of ["difficulty", "score", "username"]) {
+          var td = document.createElement("td");
+          td.appendChild(document.createTextNode(row[field]));
+          tr.appendChild(td);
+        }
+        leaderboard.appendChild(tr);
+      }
+    }
+  };
+  xhttp.send();
+}
