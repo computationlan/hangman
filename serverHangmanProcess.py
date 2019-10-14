22172f482ea9c8b2a4aacda649fff3960e09dac3
Lidia <computationlan@gmail.com>
2019-10-13 19:29:57 -0700

    added share with a friend link

--- serverHangmanProcess.py
+++ serverHangmanProcess.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request, redirect, session, url_for, escape
+from flask import Flask, request, redirect, session, url_for, escape, render_template
 from flask.json import jsonify
 import requests
 import random
@@ -12,7 +12,7 @@ app.secret_key = b'qz~y\9K_CU>&uGYM;xcW'
 @app.route('/')
 def main_page():
     path = "game_of_hangman.html"
-    return app.send_static_file(path)
+    return render_template(path)
 
 @app.route('/game_init', methods=['GET'])
 def game_init():
@@ -20,6 +20,19 @@ def game_init():
     if request.method == 'GET':
         username = request.args.get('username')
         difficulty = request.args.get('difficulty')
+        if 'challenge_id' in request.args:
+            session['challenge_id'] = request.args.get('challenge_id')
+            conn = sqlite3.connect('hangman.db')
+            cur = conn.cursor()
+            cur.execute('SELECT challenge, difficulty, score, username FROM challenge_share WHERE share_id = ?', (session['challenge_id'],))
+            row = cur.fetchall()
+            session['challenge'] = row[0][0]
+            session['difficulty'] = row[0][1]
+            session['friend_score'] = row[0][2]
+        else:
+            print('no challenge_id is in')
+            response = requests.get("http://app.linkedin-reach.io/words?difficulty="+difficulty)
+            session['challenge'] = random.choice(response.text.split('\n')).upper()
         session['username'] = username
         session['difficulty'] = difficulty
         session['wrong_guesses_left'] = 6
@@ -28,11 +41,9 @@ def game_init():
         #print(len(response.text))
 
         #add some error message if nothing is returned on the request - either connection issue or there are no words
-        #send leaderboard
         #keyboard should also respond to real actual keyboard, not just on screen buttons
 
         #print(random.choice(response.text.split('\n')))
-        session['challenge'] = 'MANAGES'
         session['guessed'] = ['*']*len(session['challenge'])
         session['wrong_guesses'] = []
         game_json = {'guessed': session['guessed'], 'wrong_guesses_left': session['wrong_guesses_left'], 'game_over':False}
@@ -45,6 +56,7 @@ def guess_handler():
         letter = request.args.get('letterGuessed')
         print('begin', letter, session)
         game_over = False
+        share_id = None
         game_over_message = ''
         if letter not in session['challenge'] and letter not in session['wrong_guesses']:
             session['wrong_guesses_left'] -= 1
@@ -64,17 +76,24 @@ def guess_handler():
                 game_over = True
                 game_over_message = 'Awesome! You won!'
                 conn = sqlite3.connect('hangman.db')
-                sql = ''' UPDATE leaderboard SET score = ? ,
-                  username = ? 
-              WHERE difficulty = ? and score < ? '''
+                sql = ' UPDATE leaderboard SET score = ? , username = ? WHERE difficulty = ? and score < ? '
                 cur = conn.cursor()
                 cur.execute(sql, (100*session['wrong_guesses_left'], session['username'], session['difficulty'], 100*session['wrong_guesses_left']))
                 conn.commit()
-                print('sql updated')
+
+                conn = sqlite3.connect('hangman.db')
+                sql = ' INSERT INTO challenge_share (challenge, difficulty, score, username) VALUES(?,?,?,?) '
+                cur = conn.cursor()
+                cur.execute(sql, (session['challenge'], session['difficulty'], 100*session['wrong_guesses_left'], session['username']))
+                share_id = cur.lastrowid
+                conn.commit()
         print(session)
     guess_json = {'guessed': session['guessed'], 'wrong_guesses_left': session['wrong_guesses_left'], 'wrong_guesses': session['wrong_guesses'], 'game_over':game_over, 'game_over_message':game_over_message}
     if game_over:
-        guess_json['guessed'] = list(session['challenge'])
+        guess_json['challenge'] = session['challenge']
+    if share_id:
+        guess_json['share_id'] = share_id
+        guess_json['friend_name'] = session['username']
     return json.dumps(guess_json)#'Your guess was ' + str(letter)
 
 @app.route('/leaderboard')
